9f8c544e5ad10d2f7dee9635622d4a48a9a1cbee
Killianhbr <78799633+Killianhbr@users.noreply.github.com>
2021-03-23 15:59:44 +0100

    A
    Avencement

--- src/BrainCap/BrainCap/frmReactionTime.cs
+++ src/BrainCap/BrainCap/frmReactionTime.cs
@@ -12,18 +12,56 @@ namespace BrainCap
 {
     public partial class frmReactionTime : Form
     {
+        private int startseconds;
+
+        private DateTime startReactionTime;
+        private DateTime endReactionTime;
+        private double elapseMilliseconds;
+
+        private int sequence;
         public frmReactionTime()
         {
             InitializeComponent();
+            sequence = 0;
         }
 
         private void frmReactionTime_Click(object sender, EventArgs e)
         {
-            Image myimage = new Bitmap(@"C:\Users\Administrateur\Documents\GitHub\BrainCap_Git\src\BrainCap\BrainCap\ressources\img\RTWait.png");
-            this.BackgroundImage = myimage;
-            tmrRdm.Start();
-        }
+            switch (sequence)
+            {
+                case 0:// Start phase
+                    {
+                        Image myimage = new Bitmap(@"C:\Users\Administrateur\Documents\GitHub\BrainCap_Git\src\BrainCap\BrainCap\ressources\img\RTWait.png");
+                        this.BackgroundImage = myimage;
 
+                        Random ran = new Random();
+                        startseconds = ran.Next(1000, 5000);
+                        System.Threading.Tasks.Task.Delay(startseconds).ContinueWith((_) => StartReaction());
+                        ++sequence;
+                        break;
+                    }
+                case 1:// Reaction phase
+                    {
+                        endReactionTime = DateTime.Now;
+                        Image myimage = new Bitmap(@"C:\Users\Administrateur\Documents\GitHub\BrainCap_Git\src\BrainCap\BrainCap\ressources\img\RTResultat.png");
+                        this.BackgroundImage = myimage;
+
+                        TimeSpan sp = endReactionTime - startReactionTime;
+                        elapseMilliseconds = sp.TotalMilliseconds;
+                        ++sequence;
+                        break;
+                    }
+            }
+
+                    //tmrRdm.Start();
+            }
+
+        private void StartReaction()
+        {
+            Image myimage = new Bitmap(@"C:\Users\Administrateur\Documents\GitHub\BrainCap_Git\src\BrainCap\BrainCap\ressources\img\RTClick.png");
+            this.BackgroundImage = myimage;
+            startReactionTime = DateTime.Now;
+        }
         private void frmReactionTime_Load(object sender, EventArgs e)
         {
 
